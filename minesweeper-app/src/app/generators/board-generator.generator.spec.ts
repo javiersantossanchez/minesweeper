045c2675bcfb3fca70a62376bda98edc5897f4f8
javier santos <javier.david.santos@gmail.com>
2020-03-19 14:52:45 -0500

    add more unit test

--- minesweeper-app/src/app/generators/board-generator.generator.spec.ts
+++ minesweeper-app/src/app/generators/board-generator.generator.spec.ts
@@ -29,12 +29,13 @@ describe('BoardGenerator', () => {
     expect(numberOfMinesResult).toEqual(numberOfMinesExpected);
   });
 
-  it('install more mines than square available', () => {
+  it('install zero mines', () => {
+
     const generator = new BoardGenerator();
 
-    const length = 3;
+    const length = 5;
     const initialSquare: Array<Array<Square>> = new Array<Array<Square>>();
-    const numberOfMinesExpected = 1;
+    const numberOfMinesExpected = 0;
 
     for (let indexRow = 1; indexRow <= length; indexRow++) {
       const row: Array<Square> = new Array<Square>();
@@ -43,9 +44,50 @@ describe('BoardGenerator', () => {
         row.push(square);
       }
       initialSquare.push(row);
-
-      generator.installMines(initialSquare, numberOfMinesExpected);
     }
+    const boardResult: Array<Array<Square>> = generator.installMines(initialSquare, numberOfMinesExpected);
+    const numberOfMinesResult =
+        boardResult.map( row => row.filter(square => square.isMine())).map( row => row.length).reduce((sum, current) => sum + current , 0);
+
+    expect(numberOfMinesResult).toEqual(numberOfMinesExpected);
+  });
+
+  it('install more mines than square available', () => {
+    const generator = new BoardGenerator();
+
+    const length = 3;
+    const numberOfMinesExpected = (length * length) + 1;
+    const initialSquare: Array<Array<Square>> = new Array<Array<Square>>();
+
+    for (let indexRow = 1; indexRow <= length; indexRow++) {
+      const row: Array<Square> = new Array<Square>();
+      for (let indexColumn = 1; indexColumn <= length; indexColumn++) {
+        const square: Square = new Square(0, indexRow, indexColumn, STATUS.CLOSED, false, false, 0);
+        row.push(square);
+      }
+      initialSquare.push(row);
+    }
+    const boardResult: Array<Array<Square>> = generator.installMines(initialSquare, numberOfMinesExpected);
+    expect(boardResult).toBeNull();
+  });
+
+  it('install a negative number of mines', () => {
+    const generator = new BoardGenerator();
+
+    const length = 3;
+    const numberOfMinesExpected = -1;
+    const initialSquare: Array<Array<Square>> = new Array<Array<Square>>();
+
+    for (let indexRow = 1; indexRow <= length; indexRow++) {
+      const row: Array<Square> = new Array<Square>();
+      for (let indexColumn = 1; indexColumn <= length; indexColumn++) {
+        const square: Square = new Square(0, indexRow, indexColumn, STATUS.CLOSED, false, false, 0);
+        row.push(square);
+      }
+      initialSquare.push(row);
+    }
+    const boardResult: Array<Array<Square>> = generator.installMines(initialSquare, numberOfMinesExpected);
+    expect(boardResult).toBeNull();
   });
 
 });
--- minesweeper-app/src/app/generators/board-generator.generator.ts
+++ minesweeper-app/src/app/generators/board-generator.generator.ts
@@ -3,6 +3,9 @@ import { Square } from '../entities/square';
 export class BoardGenerator {
 
   installMines(board: Array<Array<Square>>, numberOfMineToInstall: number): Array<Array<Square>> {
+    if ((numberOfMineToInstall > board.length * board.length) || (numberOfMineToInstall < 0 )) {
+      return null;
+    }
     for (let i = 1; i <= numberOfMineToInstall; ) {
       const row = Math.floor(Math.random() * Math.floor(board.length));
       const column = Math.floor(Math.random() * Math.floor(board.length));
